94dd19a375dfbfc4c41a5d76bdfc1648e7856a4a
nbdunks <thenicoleduncan@gmail.com>
2019-11-20 12:52:57 -0700

    added require express, and the schema

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO cats (burger_name, sleepy) VALUES ('Bacon Burger', true);
+INSERT INTO cats (burger_name, sleepy) VALUES ('Vegetarian Burger', true);
+INSERT INTO cats (burger_name, sleepy) VALUES ('Smokey Burger', true);
+INSERT INTO cats (burger_name) VALUES ('Double-Double Cheeseburger');

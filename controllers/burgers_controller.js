8db047b196bdab4aace87a2962bfc450897abe92
nbdunks <thenicoleduncan@gmail.com>
2019-11-20 13:21:13 -0700

    set up in the MVC filing way

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var router = express.Router();
+
+var cat = require("../models/cat.js");
+
+// routes
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 80000;
+var PORT = process.env.PORT || 8000;
 
 var app = express();
 
